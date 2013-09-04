d718d922c7e4d2afd61ae1155e7a11dfcc922922
petehunt <floydophone@gmail.com>
2013-09-04 15:09:53 -0700

    add example

---
+++ example/src/MyComponent.js
@@ -0,0 +1,11 @@
+/** @jsx React.DOM */
+
+var myimg = require('./myimg.jpg');
+
+var MyComponent = React.createClass({
+  render: function() {
+    return <html><body><img src={myimg} /></body></html>;
+  }
+});
+
+module.exports = MyComponent;
